baf9cf2d3a0602f36cccbb5270df8c1f046d86c9
Aram2811 <116632907+Aram2811@users.noreply.github.com>
2023-04-10 18:55:49 +0330

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+c = num1
+num1 = num2
+num2 = c
+print(num1)
+print(num2)
---
+++ P4.py
@@ -0,0 +1,4 @@
+num1 = int(input())
+num2 = int(input())
+print("The total amount is ", (20 * num1) + (30 * num2) + 400, "$", ".", "400$ minimum wage + ",
+      20 * num1, "$", " for ", num1, " years experience + ", 30 * num2, "$", " for ", num2, " kids.")
